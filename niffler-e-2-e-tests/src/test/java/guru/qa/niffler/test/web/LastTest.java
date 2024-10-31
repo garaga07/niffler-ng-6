89b46d946da287ffaa2defacd3cd991c9c582bea
Serghei Garaga <garaga07@yandex.ru>
2024-10-31 17:13:46 +0200

    исправление ДЗ №13

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/LastTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/LastTest.java
@@ -3,14 +3,14 @@ package guru.qa.niffler.test.web;
 import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.model.rest.UserJson;
 import guru.qa.niffler.service.impl.UsersApiClient;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.parallel.Isolated;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-@Isolated
+@Order(Integer.MAX_VALUE)
 public class LastTest {
     @User
     @Test
