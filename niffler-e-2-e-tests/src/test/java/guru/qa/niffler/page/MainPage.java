579e837c1654680825013685b28c0a5031599a24
Serghei Garaga <garaga07@yandex.ru>
2024-11-01 17:01:25 +0200

    Выполнение дз №14

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/MainPage.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/MainPage.java
@@ -1,41 +1,55 @@
 package guru.qa.niffler.page;
 
+import com.codeborne.selenide.ElementsCollection;
 import guru.qa.niffler.page.component.Header;
 import guru.qa.niffler.page.component.SpendingTable;
 import guru.qa.niffler.page.component.StatComponent;
 import io.qameta.allure.Step;
 
 import javax.annotation.Nonnull;
+import java.util.List;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class MainPage extends BasePage<MainPage> {
 
-  public static final String URL = CFG.frontUrl() + "main";
+    public static final String URL = CFG.frontUrl() + "main";
 
-  protected final Header header = new Header();
-  protected final SpendingTable spendingTable = new SpendingTable();
-  protected final StatComponent statComponent = new StatComponent();
+    protected final Header header = new Header();
+    protected final SpendingTable spendingTable = new SpendingTable();
+    protected final StatComponent statComponent = new StatComponent();
+    // Коллекция всех ячеек статистики
+    private final ElementsCollection statisticCells = $$("#legend-container li");
 
-  @Nonnull
-  public Header getHeader() {
-    return header;
-  }
+    @Nonnull
+    public Header getHeader() {
+        return header;
+    }
 
-  @Nonnull
-  public SpendingTable getSpendingTable() {
-    spendingTable.getSelf().scrollIntoView(true);
-    return spendingTable;
-  }
+    @Nonnull
+    public SpendingTable getSpendingTable() {
+        spendingTable.getSelf().scrollIntoView(true);
+        return spendingTable;
+    }
 
-  @Step("Check that page is loaded")
-  @Override
-  @Nonnull
-  public MainPage checkThatPageLoaded() {
-    header.getSelf().should(visible).shouldHave(text("Niffler"));
-    statComponent.getSelf().should(visible).shouldHave(text("Statistics"));
-    spendingTable.getSelf().should(visible).shouldHave(text("History of Spendings"));
-    return this;
-  }
+    @Step("Check that page is loaded")
+    @Override
+    @Nonnull
+    public MainPage checkThatPageLoaded() {
+        header.getSelf().should(visible).shouldHave(text("Niffler"));
+        statComponent.getSelf().should(visible).shouldHave(text("Statistics"));
+        spendingTable.getSelf().should(visible).shouldHave(text("History of Spendings"));
+        return this;
+    }
+
+    @Step("Check that statistic cells contain texts {texts}")
+    @Nonnull
+    public MainPage checkStatisticCells(List<String> texts) {
+        for (String text : texts) {
+            statisticCells.findBy(text(text)).shouldBe(visible);
+        }
+        return this;
+    }
 }
--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/ProfileTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/ProfileTest.java
@@ -2,134 +2,160 @@ package guru.qa.niffler.test.web;
 
 import com.codeborne.selenide.Selenide;
 import guru.qa.niffler.jupiter.annotation.Category;
+import guru.qa.niffler.jupiter.annotation.ScreenShotTest;
 import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.jupiter.annotation.meta.WebTest;
 import guru.qa.niffler.model.rest.UserJson;
 import guru.qa.niffler.page.LoginPage;
 import guru.qa.niffler.page.MainPage;
 import guru.qa.niffler.page.ProfilePage;
+import guru.qa.niffler.utils.ScreenDiffResult;
 import org.junit.jupiter.api.Test;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import static com.codeborne.selenide.Selenide.$;
 import static guru.qa.niffler.utils.RandomDataUtils.randomCategoryName;
 import static guru.qa.niffler.utils.RandomDataUtils.randomName;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @WebTest
 public class ProfileTest {
 
-  @User(
-      categories = @Category(
-          archived = true
-      )
-  )
-  @Test
-  void archivedCategoryShouldPresentInCategoriesList(UserJson user) {
-    final String categoryName = user.testData().categoryDescriptions()[0];
+    @User(
+            categories = @Category(
+                    archived = true
+            )
+    )
+    @Test
+    void archivedCategoryShouldPresentInCategoriesList(UserJson user) {
+        final String categoryName = user.testData().categoryDescriptions()[0];
 
-    Selenide.open(LoginPage.URL, LoginPage.class)
-        .fillLoginPage(user.username(), user.testData().password())
-        .submit(new MainPage())
-        .checkThatPageLoaded();
+        Selenide.open(LoginPage.URL, LoginPage.class)
+                .fillLoginPage(user.username(), user.testData().password())
+                .submit(new MainPage())
+                .checkThatPageLoaded();
 
-    Selenide.open(ProfilePage.URL, ProfilePage.class)
-        .checkArchivedCategoryExists(categoryName);
-  }
+        Selenide.open(ProfilePage.URL, ProfilePage.class)
+                .checkArchivedCategoryExists(categoryName);
+    }
 
-  @User(
-      categories = @Category(
-          archived = false
-      )
-  )
-  @Test
-  void activeCategoryShouldPresentInCategoriesList(UserJson user) {
-    final String categoryName = user.testData().categoryDescriptions()[0];
+    @User(
+            categories = @Category(
+                    archived = false
+            )
+    )
+    @Test
+    void activeCategoryShouldPresentInCategoriesList(UserJson user) {
+        final String categoryName = user.testData().categoryDescriptions()[0];
 
-    Selenide.open(LoginPage.URL, LoginPage.class)
-        .fillLoginPage(user.username(), user.testData().password())
-        .submit(new MainPage())
-        .checkThatPageLoaded();
+        Selenide.open(LoginPage.URL, LoginPage.class)
+                .fillLoginPage(user.username(), user.testData().password())
+                .submit(new MainPage())
+                .checkThatPageLoaded();
 
-    Selenide.open(ProfilePage.URL, ProfilePage.class)
-        .checkCategoryExists(categoryName);
-  }
+        Selenide.open(ProfilePage.URL, ProfilePage.class)
+                .checkCategoryExists(categoryName);
+    }
 
-  @User
-  @Test
-  void shouldUpdateProfileWithAllFieldsSet(UserJson user) {
-    final String newName = randomName();
+    @User
+    @Test
+    void shouldUpdateProfileWithAllFieldsSet(UserJson user) {
+        final String newName = randomName();
 
-    ProfilePage profilePage = Selenide.open(LoginPage.URL, LoginPage.class)
-        .fillLoginPage(user.username(), user.testData().password())
-        .submit(new MainPage())
-        .checkThatPageLoaded()
-        .getHeader()
-        .toProfilePage()
-        .uploadPhotoFromClasspath("img/cat.jpeg")
-        .setName(newName)
-        .submitProfile()
-        .checkAlertMessage("Profile successfully updated");
+        ProfilePage profilePage = Selenide.open(LoginPage.URL, LoginPage.class)
+                .fillLoginPage(user.username(), user.testData().password())
+                .submit(new MainPage())
+                .checkThatPageLoaded()
+                .getHeader()
+                .toProfilePage()
+                .uploadPhotoFromClasspath("img/cat.png")
+                .setName(newName)
+                .submitProfile()
+                .checkAlertMessage("Profile successfully updated");
 
-    Selenide.refresh();
+        Selenide.refresh();
 
-    profilePage.checkName(newName)
-        .checkPhotoExist();
-  }
+        profilePage.checkName(newName)
+                .checkPhotoExist();
+    }
 
-  @User
-  @Test
-  void shouldUpdateProfileWithOnlyRequiredFields(UserJson user) {
-    final String newName = randomName();
+    @User
+    @Test
+    void shouldUpdateProfileWithOnlyRequiredFields(UserJson user) {
+        final String newName = randomName();
 
-    ProfilePage profilePage = Selenide.open(LoginPage.URL, LoginPage.class)
-        .fillLoginPage(user.username(), user.testData().password())
-        .submit(new MainPage())
-        .checkThatPageLoaded()
-        .getHeader()
-        .toProfilePage()
-        .setName(newName)
-        .submitProfile()
-        .checkAlertMessage("Profile successfully updated");
+        ProfilePage profilePage = Selenide.open(LoginPage.URL, LoginPage.class)
+                .fillLoginPage(user.username(), user.testData().password())
+                .submit(new MainPage())
+                .checkThatPageLoaded()
+                .getHeader()
+                .toProfilePage()
+                .setName(newName)
+                .submitProfile()
+                .checkAlertMessage("Profile successfully updated");
 
-    Selenide.refresh();
+        Selenide.refresh();
 
-    profilePage.checkName(newName);
-  }
+        profilePage.checkName(newName);
+    }
 
-  @User
-  @Test
-  void shouldAddNewCategory(UserJson user) {
-    String newCategory = randomCategoryName();
+    @User
+    @Test
+    void shouldAddNewCategory(UserJson user) {
+        String newCategory = randomCategoryName();
 
-    Selenide.open(LoginPage.URL, LoginPage.class)
-        .fillLoginPage(user.username(), user.testData().password())
-        .submit(new MainPage())
-        .checkThatPageLoaded()
-        .getHeader()
-        .toProfilePage()
-        .addCategory(newCategory)
-        .checkAlertMessage("You've added new category:")
-        .checkCategoryExists(newCategory);
-  }
+        Selenide.open(LoginPage.URL, LoginPage.class)
+                .fillLoginPage(user.username(), user.testData().password())
+                .submit(new MainPage())
+                .checkThatPageLoaded()
+                .getHeader()
+                .toProfilePage()
+                .addCategory(newCategory)
+                .checkAlertMessage("You've added new category:")
+                .checkCategoryExists(newCategory);
+    }
 
-  @User(
-      categories = {
-          @Category(name = "Food"),
-          @Category(name = "Bars"),
-          @Category(name = "Clothes"),
-          @Category(name = "Friends"),
-          @Category(name = "Music"),
-          @Category(name = "Sports"),
-          @Category(name = "Walks"),
-          @Category(name = "Books")
-      }
-  )
-  @Test
-  void shouldForbidAddingMoreThat8Categories(UserJson user) {
-    Selenide.open(LoginPage.URL, LoginPage.class)
-        .fillLoginPage(user.username(), user.testData().password())
-        .submit(new MainPage())
-        .checkThatPageLoaded()
-        .getHeader()
-        .toProfilePage()
-        .checkThatCategoryInputDisabled();
-  }
+    @User(
+            categories = {
+                    @Category(name = "Food"),
+                    @Category(name = "Bars"),
+                    @Category(name = "Clothes"),
+                    @Category(name = "Friends"),
+                    @Category(name = "Music"),
+                    @Category(name = "Sports"),
+                    @Category(name = "Walks"),
+                    @Category(name = "Books")
+            }
+    )
+    @Test
+    void shouldForbidAddingMoreThat8Categories(UserJson user) {
+        Selenide.open(LoginPage.URL, LoginPage.class)
+                .fillLoginPage(user.username(), user.testData().password())
+                .submit(new MainPage())
+                .checkThatPageLoaded()
+                .getHeader()
+                .toProfilePage()
+                .checkThatCategoryInputDisabled();
+    }
+
+    @User
+    @ScreenShotTest(value = "img/profile-expected.png")
+    void checkProfileImageTest(UserJson user, BufferedImage expected) throws IOException {
+        Selenide.open(LoginPage.URL, LoginPage.class)
+                .fillLoginPage(user.username(), user.testData().password())
+                .submit(new MainPage())
+                .checkThatPageLoaded()
+                .getHeader()
+                .toProfilePage()
+                .uploadPhotoFromClasspath("img/cat.png")
+                .submitProfile();
+        BufferedImage actual = ImageIO.read($(".MuiAvatar-img").screenshot());
+        assertFalse(new ScreenDiffResult(
+                actual,
+                expected
+        ));
+    }
 }
