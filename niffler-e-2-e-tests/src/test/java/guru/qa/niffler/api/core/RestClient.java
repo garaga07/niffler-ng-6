427e9fb7a50379e3084eb53753374dc6dd16521f
Serghei Garaga <garaga07@yandex.ru>
2024-11-05 10:24:02 +0200

    синхронизация с форком

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/core/RestClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/core/RestClient.java
@@ -1,6 +1,7 @@
 package guru.qa.niffler.api.core;
 
 import guru.qa.niffler.config.Config;
+import io.qameta.allure.okhttp3.AllureOkHttp3;
 import okhttp3.Interceptor;
 import okhttp3.JavaNetCookieJar;
 import okhttp3.OkHttpClient;
