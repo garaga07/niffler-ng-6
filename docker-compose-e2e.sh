afae52b040772844291ad633ca2fc0cb8b43cc5a
Serghei Garaga <garaga07@yandex.ru>
2025-02-06 12:22:13 +0200

    Выполнение ДЗ № 28

--- docker-compose-e2e.sh
+++ docker-compose-e2e.sh
@@ -7,29 +7,58 @@ export ALLURE_DOCKER_API=http://allure:5050/
 export HEAD_COMMIT_MESSAGE="local build"
 export ARCH=$(uname -m)
 
+# Значения по умолчанию
+BROWSER="chrome"
+SKIP_BUILD=false
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    chrome|firefox)
+      BROWSER="$1"
+      ;;
+    --skip-build)
+      SKIP_BUILD=true
+      ;;
+  esac
+  shift
+done
+
+echo "### Selected browser: $BROWSER ###"
+export SELENOID_BROWSER=$BROWSER
+
+echo "### Skip build: $SKIP_BUILD ###"
+
 echo '### Java version ###'
 java --version
 
-docker compose down
-docker_containers=$(docker ps -a -q)
-docker_images=$(docker images --format '{{.Repository}}:{{.Tag}}' | grep 'niffler')
+if [ "$SKIP_BUILD" = false ]; then
+  echo "### Stopping and removing old containers ###"
+  docker compose down
+  docker_containers=$(docker ps -a -q)
 
-if [ ! -z "$docker_containers" ]; then
-  echo "### Stop containers: $docker_containers ###"
-  docker stop $docker_containers
-  docker rm $docker_containers
+  if [ -n "$docker_containers" ]; then
+    docker stop $docker_containers
+    docker rm $docker_containers
+  fi
+
+  docker_images=$(docker images --format '{{.Repository}}:{{.Tag}}' | grep 'niffler')
+  if [ -n "$docker_images" ]; then
+    echo "### Removing images: $docker_images ###"
+    docker rmi $docker_images
+  fi
+
+  echo "### Running build ###"
+  bash ./gradlew clean
+  bash ./gradlew jibDockerBuild -x :niffler-e-2-e-tests:test
+
+  echo "### Starting all containers ###"
+  docker compose up -d
+else
+  echo "### Skipping build and image cleanup ###"
+
+  echo "### Recreating test container with new browser ###"
+  docker compose rm -f niffler-e-2-e
+  docker compose up -d niffler-e-2-e
 fi
 
-if [ ! -z "$docker_images" ]; then
-  echo "### Remove images: $docker_images ###"
-  docker rmi $docker_images
-fi
-
-echo '### Java version ###'
-java --version
-bash ./gradlew clean
-bash ./gradlew jibDockerBuild -x :niffler-e-2-e-tests:test
-
-docker pull selenoid/vnc_chrome:127.0
-docker compose up -d
 docker ps -a
