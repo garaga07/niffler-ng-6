1cfab8b7d51873bbc96698e23f68c4e427a2d77a
Serghei Garaga <garaga07@yandex.ru>
2025-03-03 08:28:36 +0200

    правка замечаний ревьюера по ДЗ к уроку 21.2

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/kafka/UserdataKafkaTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/kafka/UserdataKafkaTest.java
@@ -4,18 +4,22 @@ import guru.qa.niffler.api.AuthApi;
 import guru.qa.niffler.api.core.RestClient;
 import guru.qa.niffler.api.core.ThreadSafeCookieStore;
 import guru.qa.niffler.config.Config;
+import guru.qa.niffler.data.repository.impl.UserdataUserRepositoryHibernate;
 import guru.qa.niffler.jupiter.annotation.meta.KafkaTest;
 import guru.qa.niffler.model.rest.UserJson;
-import guru.qa.niffler.utils.DatabaseWaiter;
+import guru.qa.niffler.utils.Waiter;
 import guru.qa.niffler.utils.RandomDataUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
 
 @KafkaTest
 public class UserdataKafkaTest {
 
     private static final Config CFG = Config.getInstance();
     private final AuthApi authApi = new RestClient.EmptyClient(CFG.authUrl()).create(AuthApi.class);
+    private final UserdataUserRepositoryHibernate userRepository = new UserdataUserRepositoryHibernate();
 
     @Test
     void userShouldBeSavedToDatabaseAfterKafkaProcessing() throws Exception {
@@ -27,7 +31,12 @@ public class UserdataKafkaTest {
         authApi.register(username, password, password, ThreadSafeCookieStore.INSTANCE.cookieValue("XSRF-TOKEN")).execute();
 
         // Ожидаем появления пользователя в БД `userdata`
-        UserJson userFromDb = DatabaseWaiter.waitForUserInDatabase(username, 5000L);
+        UserJson userFromDb = Waiter.waitForCondition(
+                () -> userRepository.findByUsername(username)
+                        .map(u -> UserJson.fromEntity(u, null))
+                        .orElse(null),
+                5000L
+        );
 
         // Проверяем, что пользователь появился в `userdata`
         Assertions.assertNotNull(userFromDb, "Пользователь не найден в БД `userdata`");
